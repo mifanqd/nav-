3735c5527c5f93f50824148343fa859a7cf91022
无铭 <123754199+mifanqd@users.noreply.github.com>
2024-11-30 23:00:09 +0800

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -22,17 +22,24 @@ import { zh_CN } from 'ng-zorro-antd/i18n'
 
 registerLocaleData(zh)
 
-const icons: IconDefinition[] = [
-  CheckOutline,
-  CopyOutline,
-  ShareAltOutline,
-  EllipsisOutline,
-  LoadingOutline,
-  UploadOutline,
-  MinusOutline,
-  PlusOutline,
-  StopOutline,
-]
+const icons: const WEBSITE_SORTS: WebsiteSort[] = [
+  {
+    title: "示例分类1",
+    sites: [
+      {
+        /** 网站标题 */
+        title: "主标题",
+        /** 网站 url */
+        url: "http://www.your-web-page.com/",
+        /** 网站 icon 图标，非必填，留空默认展示网站标题的第一个字符 */
+        icon: "",
+        /** 指定网站 icon 背景色 */
+        color: "#0171CD",
+      },
+    ],
+  },
+  // ... ...
+];
 
 export const appConfig: ApplicationConfig = {
   providers: [
